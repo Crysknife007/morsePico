81ebbd1cf931fe18408ac6f733344a3e86becf2c
Spike Snell <spikesnell@gmail.com>
2021-08-08 19:07:57 -0500

    Added autospacing feature

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 # One Button Morse Code - HID USB Keyboard 
 # Using CircuitPython and Raspberry Pi Pico
 # Gboard definitons included for special characters
-# 08.04.2021 Spike Snell 
+# 08.08.2021 Spike Snell 
 
 # Import the required abilities
 from time import sleep
@@ -28,6 +28,13 @@ freq = 4444
 # Set buzzer mute to false by default
 # Entering SHIFT ( ....-. ) twice in a row toggles this value
 mute = False
+
+# Set auto spacing to false by default
+# Entering SHIFT ( ....-. ) and then SPACE ( ..-- ) toggles auto entry of space
+autoSpace = False
+
+# Set last key printable flag to false by default
+lastKeyPrintable = False
 
 # Define the keyboard
 keyboard = Keyboard(devices)
@@ -218,6 +225,9 @@ while True:
                     # If the current key entered was a shift
                     if CurrentKey == '....-.':
 
+                        # Set last key printable flag back to false
+                        lastKeyPrintable = False
+
                         # If shift was entered a second time 
                         if shiftMode == True:
                         
@@ -233,6 +243,15 @@ while True:
                             # Set shift mode to true
                             shiftMode = True
 
+                    # If the current key entered was space
+                    if CurrentKey == '..--':
+
+                        # If we are already in shift mode
+                        if shiftMode == True:
+                        
+                            # Toggle auto space
+                            autoSpace = not autoSpace
+
                     # If we actually found the key in our dictionary
                     if key != '':
                        
@@ -242,17 +261,41 @@ while True:
                             # Write the current key on the us layout
                             kl.write(key)
 
+                            # Check to see if we can set last char printable flag
+                            if ( ( key != ' ' ) and ( key != '\n' ) and ( key != '\b' ) ):
+                            
+                                # Set last key printable flag to true
+                                lastKeyPrintable = True
+
                         # Else we are in shift mode
                         else:
 
-                            # Write the current key uppercased on the us layout
-                            kl.write(key.upper())
-                                
                             # Set shift mode to false
                             shiftMode = False
 
+                            # Check to see if we can set last char printable flag
+                            if ( ( key != ' ' ) and ( key != '\n' ) and ( key != '\b' ) ):
+
+                                # Write the current key uppercased on the us layout
+                                kl.write(key.upper())
+                                
+                                # Set last key printable flag to true
+                                lastKeyPrintable = True
+
                     # Set the current letter back to an empty string    
                     CurrentKey = ''
+
+                # If we need to enter a space here
+                if DelayCount > ( dit * mult ) * 3:
+
+                    # If auto spacing is true and the last key was printable
+                    if ( ( autoSpace == True ) and ( lastKeyPrintable == True ) ) :
+
+                        # Write a space to the screen
+                        kl.write(' ')
+
+                        # Set last key printable flag back to false
+                        lastKeyPrintable = False
 
         # Sleep one unit of time            
         sleep(0.01)
